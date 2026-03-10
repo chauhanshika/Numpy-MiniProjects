a6ee1a68b0d74603dca7ec225ee79131457c282f
Anshika Chauhan <chauhanshikaa1@gmail.com>
2026-03-11 00:23:50 +0530

    Add product sales and monthly sales calculations

--- Project 3  Sales Data Analyzer/main.py
+++ Project 3  Sales Data Analyzer/main.py
@@ -8,6 +8,14 @@ sales = np.array([
 
 total_sales = np.sum(sales)
 region_sales = np.sum(sales, axis=(0,2))
+product_sales = np.sum(sales, axis=(1,2))
+best_product = np.argmax(product_sales)
+monthly_sales = np.sum(sales, axis=(0,1))
+
+print("\nSales Data Analysis\n")
 
 print("Total Sales:", total_sales)
 print("Sales per Region:", region_sales)
+print("Sales per Product:", product_sales)
+print("Best Selling Product Index:", best_product)
+print("Monthly Sales:", monthly_sales)
