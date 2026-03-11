9df89fbd21c4d2eb9710112aaa627407cf37c6be
Anshika Chauhan <chauhanshikaa1@gmail.com>
2026-03-11 23:29:53 +0530

    Add simulated grayscale image to main.py

---
+++ Project-4-Image-Processor/main.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Simulated grayscale image (5x5 pixel matrix)
+image = np.array([
+    [50, 80, 90, 120, 150],
+    [60, 70, 100, 130, 160],
+    [40, 60, 110, 140, 170],
+    [30, 50, 90, 150, 180],
+    [20, 40, 80, 140, 200]
+])
+
+print("Original Image Matrix:\n")
+print(image)
