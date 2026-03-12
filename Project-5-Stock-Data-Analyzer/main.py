e92d0b07468c51245cb83b81d7e6aa694ad9acf7
Anshika Chauhan <chauhanshikaa1@gmail.com>
2026-03-12 22:15:52 +0530

    Enhance stock data analysis with new metrics
    Added calculations for average price, max price, min price, daily returns, and price volatility.

--- Project-5-Stock-Data-Analyzer/main.py
+++ Project-5-Stock-Data-Analyzer/main.py
@@ -1,4 +1,21 @@
 import numpy as np
 
 prices = np.array([100, 102, 101, 105, 107, 106, 108, 110, 109, 111])
-print("Stock Prices:\n", prices)
+
+avg_price = np.mean(prices)
+max_price = np.max(prices)
+min_price = np.min(prices)
+
+
+returns = np.diff(prices)
+volatility = np.std(prices)
+
+print("\nStock Data Analysis\n")
+
+print("Prices:", prices)
+print("\nAverage Price:", avg_price)
+print("Highest Price:", max_price)
+print("Lowest Price:", min_price)
+
+print("\nDaily Returns:", returns)
+print("Price Volatility:", volatility)
