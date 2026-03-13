413087d729b4693e2d2afadf7ae8cd180f60a417
Anshika Chauhan <chauhanshikaa1@gmail.com>
2026-03-14 01:26:07 +0530

    Add linear regression predictions and MSE calculation

--- Project-7-Linear-Regression/main.py
+++ Project-7-Linear-Regression/main.py
@@ -1,6 +1,17 @@
 import numpy as np
+
 x = np.array([1, 2, 3, 4, 5])
 y = np.array([50, 55, 65, 70, 75])
 
-print("Input (Hours Studied):", x)
-print("Output (Exam Scores):", y)
+m = 5
+b = 45
+
+predictions = m * x + b
+
+mse = np.mean((y - predictions) ** 2)
+
+print("\nLinear Regression Analysis\n")
+
+print("Actual Scores:", y)
+print("Predicted Scores:", predictions)
+print("\nMean Squared Error:", mse)
